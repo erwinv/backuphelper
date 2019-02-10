84b1543c8360f792960336a7af06bd8df16e7ee0
Erwin Villejo <erwin.villejo@gmail.com>
2019-02-11 06:29:52 +0800

    restored non-unified impls

--- index.js
+++ index.js
@@ -14,6 +14,11 @@ const readdirAsync = Promise.promisify(fs.readdir)
 const statAsync = Promise.promisify(fs.stat)
 const readFileAsync = R.curryN(2, Promise.promisify(fs.readFile))(_, 'utf8')
 
+const globMatch = R.compose(
+    R.anyPass,
+    R.map(R.compose(R.test, globToRegExp(_, {flags: 'i', extended: true})))
+)
+
 const readFirstLineAsync = (filepath, encoding='utf8') => new Promise((resolve, reject) => {
     const readStream = fs.createReadStream(filepath, encoding)
         .on('error', reject)
@@ -80,11 +85,6 @@ function getBackupPaths(variant='reactive') {
         Bacon.fromArray,
         R.identity
     ]
-
-    const globMatch = R.compose(
-        R.anyPass,
-        R.map(R.compose(R.test, globToRegExp(_, {flags: 'i', extended: true})))
-    )
 
     function _getBackupPaths(dir, parentpolicy='branch', parentignorepatterns=[]) {
         const policyP = Promise.resolve(
@@ -160,8 +160,107 @@ function getBackupPaths(variant='reactive') {
     }
 }
 
-const getBackupPathsAsync = getBackupPaths('async')
-const getBackupPathsRx = getBackupPaths('reactive')
+function getBackupPathsAsync(dir, parentpolicy='branch', parentignorepatterns=[]) {
+    const policyP = Promise.resolve(
+        parentpolicy == 'branch' ?
+            getBackupPolicyAsync(dir) :
+            parentpolicy
+    )
+    const patternsP = Promise.resolve(
+        parentpolicy == 'branch' ?
+            getIgnorePatternsAsync(dir) :
+            parentignorepatterns
+    )
+    return Promise.join(policyP, patternsP, (policy, ignorepatterns) =>
+        policy == 'ignore' ? Promise.resolve([]) :
+        R.call(R.pipe(
+            R.always(dir),
+            readdirAsync,
+            R.otherwise(R.always([])),
+            R.then(R.pipe(
+                R.filter(R.compose(R.not, globMatch(ignorepatterns))),
+                R.map(R.pipe(
+                    base => ({dir, base}),
+                    path.format,
+                    path.normalize
+                )),
+                R.filter(R.compose(R.not, globMatch(ignorepatterns)))
+            )),
+            R.then(R.pipe(
+                R.map(fullpath => R.call(R.pipe(
+                    R.always(fullpath),
+                    statAsync,
+                    R.otherwise(R.always({isFile: R.F, isDirectory: R.F})),
+                    R.then(stats => ({fullpath, stats}))
+                ))),
+                Promise.all
+            )),
+            R.then(R.pipe(
+                R.map(({fullpath, stats}) =>
+                    stats.isFile() ? Promise.resolve([fullpath]) :
+                    stats.isDirectory() ? getBackupPathsAsync(fullpath, policy, ignorepatterns) :
+                    Promise.resolve([])
+                ),
+                Promise.all,
+                R.then(R.flatten)
+            ))
+        ))
+    )
+}
+
+function getBackupPathsRx(dir, parentpolicy='branch', parentignorepatterns=[]) {
+    const policyP = Promise.resolve(
+        parentpolicy == 'branch' ?
+            getBackupPolicyAsync(dir) :
+            parentpolicy
+    )
+    const patternsP = Promise.resolve(
+        parentpolicy == 'branch' ?
+            getIgnorePatternsAsync(dir) :
+            parentignorepatterns
+    )
+    const policyAndPatternsP = Promise.join(policyP, patternsP,
+        (policy, ignorepatterns) => ({policy, ignorepatterns}))
+
+    const flatMap = R.invoker(1, 'flatMap')
+    const flatMapError = R.invoker(1, 'flatMapError')
+
+    return R.call(R.pipe(
+        R.always(policyAndPatternsP),
+        Bacon.fromPromise,
+        flatMap(({policy, ignorepatterns}) =>
+            policy == 'ignore' ? Bacon.never() :
+            R.call(R.pipe(
+                R.always(dir),
+                readdirAsync,
+                Bacon.fromPromise,
+                flatMapError(R.always(Bacon.never())),
+                flatMap(R.pipe(
+                    R.filter(R.compose(R.not, globMatch(ignorepatterns))),
+                    R.map(R.pipe(
+                        base => ({dir, base}),
+                        path.format,
+                        path.normalize
+                    )),
+                    R.filter(R.compose(R.not, globMatch(ignorepatterns))),
+                    Bacon.fromArray
+                )),
+                flatMap(fullpath => R.call(R.pipe(
+                    R.always(fullpath),
+                    statAsync,
+                    R.otherwise(R.always({isFile: R.F, isDirectory: R.F})),
+                    R.then(stats => ({fullpath, stats})),
+                    Bacon.fromPromise
+                ))),
+                flatMap(({fullpath, stats}) =>
+                    stats.isFile() ? Bacon.once(fullpath) :
+                    stats.isDirectory() ? getBackupPathsRx(fullpath, policy, ignorepatterns) :
+                    Bacon.never()
+                )
+            ))
+        )
+    ))
+}
 
 Object.assign(module.exports, {
     getBackupPolicyAsync,
@@ -183,14 +282,29 @@ if (runningAsMain) {
     // silent (non-eager logging) means dump all results at the very end,
     //      i.e., complete the whole list before starting printing
     // verbose (eager logging) means dump results immediately,
-    //      i.e., start printing partial list
+    //      i.e., immediately print partial results
     // --reactive supports both eager and non-eager logging
     // --async doesn't support eager logging (go figure)
 
-    const variant = useReactive ? 'reactive' : 'async'
-    const monad = getBackupPaths(variant)(dir)
-    if (eagerLogging)
-        monad.onValue(console.log)
-    else
-        monad.then(R.forEach(console.log))
+    const useUnifiedImpl = opts.includes('--unified')
+    if (useUnifiedImpl) {
+        const variant = useReactive ? 'reactive' : 'async'
+        const monad = getBackupPaths(variant)(dir)
+        return eagerLogging ?
+            monad.onValue(console.log) :
+            monad.then(R.forEach(console.log))
+    }
+
+    if (!useReactive) {
+        return getBackupPathsAsync(dir)
+            .then(R.forEach(console.log))
+    }
+
+    return eagerLogging ?
+        getBackupPathsRx(dir)
+            .onValue(console.log) :
+        getBackupPathsRx(dir)
+            .reduce([], R.flip(R.append))
+            .toPromise(Promise)
+            .then(R.forEach(console.log))
 }
